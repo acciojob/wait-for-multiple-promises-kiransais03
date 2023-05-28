c439863a81e73d575248a1cf44f2d9515c1fe836
Guduru Kiran Sai <119746361+kiransais03@users.noreply.github.com>
2023-05-28 20:10:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+let tbody = document.getElementById("output");
+
+let promise 1 =new Promise(func1);
+
+function func1() {
+	setTimeout((resolve,reject,"Promise 1",2)=>{
+		 resolve(addtoui)
+	},2000)
+}
+
+function addtoui() {
+	let trow=document.createElement('tr');
+	trow.innerHTML =`<td>Loading...</td>
+			<td>Loading...</td>`
+}
