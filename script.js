76fbe66fc653fbfad4eb113bf9c3381bc761f3e0
Guduru Kiran Sai <119746361+kiransais03@users.noreply.github.com>
2023-05-28 20:36:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,38 +3,37 @@ let tbody = document.getElementById("output");
 
 let promise1 =new Promise(func1);
 
-function func1() {
-	setTimeout(()=>{resolve("Promise1","2")},2000)
-}
 
-promise1.then(function(arg1,arg2) {
-	addtoui(arg1,arg2);
+function func1() {
+	setTimeout(resolve,2000)
 }
 
  let promise2 =new Promise(func2);
 
 function func2() {
-	setTimeout(()=>{resolve("Promise2","1")},1000)
+	setTimeout(resolve,1000)
 }
 
-promise2.then(function(arg1,arg2) {
-	addtoui(arg1,arg2);
-}
 
 let promise3 =new Promise(func3);
 
 function func3() {
-	setTimeout(()=>{resolve("Promise3","3")},2000)
-}
-
-promise1.then(function(arg1,arg2) {
-	addtoui(arg1,arg2);
+	setTimeout(resolve,2000)
 }
 
 
+Promise.all([promise1,promise2,promise3]).then(addtoui)
 
-function addtoui(arg1,arg2) {
-	let trow=document.createElement('tr');
-	trow.innerHTML =`<td>${arg1}</td>
-			<td>${arg2}</td>`
+function addtoui() {
+	
+	   let trow=document.createElement('tr');
+	    trow.innerHTML =`<td>Promise 1</td>
+			<td>2</td>
+   <td>Promise 2</td>
+			<td>1</td>
+   <td>Promise 3</td>
+			<td>3</td>
+   <td>Total</td>
+			<td>${time}</td>`
+	tbody.append(trow)
 }
