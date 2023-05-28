f31fefbe6da7f4ee5ad7a187a9eef516bbc7508c
Guduru Kiran Sai <119746361+kiransais03@users.noreply.github.com>
2023-05-28 20:22:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,40 @@
 //your JS code here. If required.
 let tbody = document.getElementById("output");
 
-let promise 1 =new Promise(func1);
+let promise1 =new Promise(func1);
 
 function func1() {
-	setTimeout((resolve,reject,"Promise 1",2)=>{
-		 resolve(addtoui)
-	},2000)
+	setTimeout(()=>{resolve("Promise1","2")},2000)
 }
 
-function addtoui() {
+promise1.then(function(arg1,arg2) {
+	addtoui(arg1,arg2);
+}
+
+ let promise2 =new Promise(func2);
+
+function func2() {
+	setTimeout(()=>{resolve("Promise2","1")},1000)
+}
+
+promise2.then(function(arg1,arg2) {
+	addtoui(arg1,arg2);
+}
+
+let promise3 =new Promise(func3);
+
+function func3() {
+	setTimeout(()=>{resolve("Promise3","3")},2000)
+}
+
+promise1.then(function(arg1,arg2) {
+	addtoui(arg1,arg2);
+}
+
+
+
+function addtoui(arg1,arg2) {
 	let trow=document.createElement('tr');
-	trow.innerHTML =`<td>Loading...</td>
-			<td>Loading...</td>`
+	trow.innerHTML =`<td>${arg1}</td>
+			<td>${arg2}</td>`
 }
